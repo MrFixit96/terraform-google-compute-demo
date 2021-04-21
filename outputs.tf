8d2cabfb5267689b150737ef236a3da5d3a925f2
James Anderton <james@janderton.com>
2021-04-20 21:28:59 -0500

    added outputs for ip addresses

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+output "private_ip" {
+  description = "list private ip on compute instance"
+  value       = google_compute_instance.default.*.network_interface.0.network_ip
+}
+
+output "public_ip" {
+  description = "list public ip on compute instance if there is one"
+  value       = google_compute_instance.default.*.network_interface.0.access_config.0.nat_ip
+}
+
+/*
+output "public_ssh_key" {
+  description = "The public key we inserted"
+  value       = ["${google_compute_instance.default.*.metadata}"]
+}
+*/
