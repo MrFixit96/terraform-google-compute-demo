0bd4656c3e73f5cc6fb83a770e32fbf3a8e80edb
James Anderton <james@janderton.com>
2021-04-20 21:07:21 -0500

    updated module to 2.0.3

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 // Modules
 module "compute" {
   source  = "app.terraform.io/janderton-sandbox/compute/google"
-  version = "2.0.2"
+  version = "2.0.3"
 
   instance_name = "tfe-gce-demo"
   preemptible = "true"
