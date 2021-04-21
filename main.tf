90b3387d0fea91e8094c82842c0ca9a9a10fc0e3
James Anderton <james@janderton.com>
2021-04-20 21:13:49 -0500

    added automatic_restart value and moved to using variables

--- main.tf
+++ main.tf
@@ -4,8 +4,9 @@ module "compute" {
   source  = "app.terraform.io/janderton-sandbox/compute/google"
   version = "2.0.3"
 
-  instance_name = "tfe-gce-demo"
-  preemptible = "true"
-  project = "jca-tfe-testing-1da3236b"
-  subnetwork_name = "sm-tfe-gcp-core-private"
+  instance_name = var.instance_name
+  preemptible = var.preemptible
+  automatic_restart = var.automatic_restart
+  project = var.project_id
+  subnetwork_name = var.subnetwork_name
 }
--- terraform.auto.tfvars
+++ terraform.auto.tfvars
@@ -1,6 +1,7 @@
 project = "jca-tfe-testing-1da3236b"
 region = "us-central1"
 instance_name = "tfe-gce-demo-1"
-preemptible = "true"
+preemptible = true
+automatic_restart = false
 subnetwork_name = "sm-tfe-gcp-core-private"
 
