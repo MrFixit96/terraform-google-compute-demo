c9bda474c45ef8cfbf481680e36f641031ac1011
James Anderton <james@janderton.com>
2021-04-20 21:45:21 -0500

    changed output module path

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "private_ip" {
   description = "list private ip on compute instance"
-  value       = module.compute.*.network_interface.0.network_ip
+  value       = module.compute.network_interface.0.network_ip
 }
 
 output "public_ip" {
   description = "list public ip on compute instance if there is one"
-  value       = module.compute.*.network_interface.0.access_config.0.nat_ip
+  value       = module.compute.network_interface.0.access_config.0.nat_ip
 }
 
 /*
