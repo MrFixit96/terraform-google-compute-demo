ef86ebf9a6712d1dbe498639e1a25338108eca0b
James Anderton <james@janderton.com>
2021-04-20 21:37:33 -0500

    fixed output format

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 output "private_ip" {
   description = "list private ip on compute instance"
-  value       = google_compute_instance.default.*.network_interface.0.network_ip
+  value       = module.compute.*.network_interface.0.network_ip
 }
 
 output "public_ip" {
   description = "list public ip on compute instance if there is one"
-  value       = google_compute_instance.default.*.network_interface.0.access_config.0.nat_ip
+  value       = module.compute.*.network_interface.0.access_config.0.nat_ip
 }
 
 /*
 output "public_ssh_key" {
   description = "The public key we inserted"
-  value       = ["${google_compute_instance.default.*.metadata}"]
+  value       = [module.compute.*.metadata]
 }
 */
